d58b2999e106809ee07071e1c890751a2d9bac55
Shivam Bhardwaj <shivam12332100@gmail.com>
2021-12-22 22:15:14 +0530

    Room Database and Retrofit Added

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/adapters/NewsAdapter.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.ui.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsAdapter{}/*:RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+    inner class ArticleViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+
+
+}*/
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/api/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.ui.api
+
+class Constants {
+    companion object
+    {
+        const val API_KEY="64dc130533c54739ac5e62c13b93ee63"
+        const val BASE_URL="https://newsapi.org"
+    }
+}
